50a0c69e26087e8e66a8377ff900a8ca55298e56
Augusto Dalla Ragione <54884671+ADR1993@users.noreply.github.com>
2022-02-11 15:38:16 +0100

    minor imputing update

--- code/process_SU_data .R
+++ code/process_SU_data .R
@@ -218,7 +218,7 @@ indiv$Age[which(is.na(indiv$Age))] <- median(indiv$Age, na.rm=TRUE)
 indiv$BMI[which(is.na(indiv$BMI))] <- median(indiv$BMI, na.rm=TRUE) 
 
 # Impute missing values of categorical variables with "unknown"
-indiv <- indiv %>% mutate_at(vars(3:4, 8:11), ~replace(., is.na(.), "UNKNOWN"))
+indiv <- indiv %>% mutate_at(vars(3:4, Grip, 8:11), ~replace(., is.na(.), "UNKNOWN"))
 
 # Create compposite religiousness variable
 indiv$RelPub <- ifelse(indiv$ReligionPublic == "AFEWTIMESPERWEEK" | indiv$ReligionPublic=="MORETHANONCEPERWEEK" | indiv$ReligionPublic == "ONCEPERWEEK", 1, 2) 
